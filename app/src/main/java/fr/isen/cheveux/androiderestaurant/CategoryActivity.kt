d7d53da6792fa88a75d66bd90fad0e9f78be230d
math-cheveux <math.cheveux@gmail.com>
2022-03-03 11:44:42 +0100

    II.f

--- app/src/main/java/fr/isen/cheveux/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/cheveux/androiderestaurant/CategoryActivity.kt
@@ -1,11 +1,17 @@
 package fr.isen.cheveux.androiderestaurant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.apply { text = message }
     }
 }
--- app/src/main/java/fr/isen/cheveux/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/cheveux/androiderestaurant/HomeActivity.kt
@@ -1,9 +1,11 @@
 package fr.isen.cheveux.androiderestaurant
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+const val EXTRA_MESSAGE = "fr.isen.cheveux.androidrestaurant.MESSAGE"
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,12 +13,27 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         val button1 = findViewById<Button>(R.id.button1)
-        button1.setOnClickListener { Toast.makeText(applicationContext, "Entrées", Toast.LENGTH_SHORT).show() }
+        button1.setOnClickListener {
+            val intent = Intent(this, CategoryActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, button1.text.toString())
+            }
+            startActivity(intent)
+        }
 
         val button2 = findViewById<Button>(R.id.button2)
-        button2.setOnClickListener { Toast.makeText(applicationContext, "Plats", Toast.LENGTH_SHORT).show() }
+        button2.setOnClickListener {
+            val intent = Intent(this, CategoryActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, button2.text.toString())
+            }
+            startActivity(intent)
+        }
 
         val button3 = findViewById<Button>(R.id.button3)
-        button3.setOnClickListener { Toast.makeText(applicationContext, "Desserts", Toast.LENGTH_SHORT).show() }
+        button3.setOnClickListener {
+            val intent = Intent(this, CategoryActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, button3.text.toString())
+            }
+            startActivity(intent)
+        }
     }
 }
