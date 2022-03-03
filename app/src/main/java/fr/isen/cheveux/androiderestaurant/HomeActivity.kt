c53b1c91ddb1baa7e072583d728cf88036c3f4a7
math-cheveux <math.cheveux@gmail.com>
2022-03-03 11:07:41 +0100

    II.d

--- app/src/main/java/fr/isen/cheveux/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/cheveux/androiderestaurant/HomeActivity.kt
@@ -1,11 +1,22 @@
 package fr.isen.cheveux.androiderestaurant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener { Toast.makeText(applicationContext, "Entrées", Toast.LENGTH_SHORT).show() }
+
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener { Toast.makeText(applicationContext, "Plats", Toast.LENGTH_SHORT).show() }
+
+        val button3 = findViewById<Button>(R.id.button3)
+        button3.setOnClickListener { Toast.makeText(applicationContext, "Desserts", Toast.LENGTH_SHORT).show() }
     }
 }
